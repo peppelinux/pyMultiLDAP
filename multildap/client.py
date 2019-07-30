02d5b7ee2ff6b653d14f4983925934ed95a947e9
root <root@auth.unical.it>
2019-07-30 16:21:07 +0200

    connect timeout fixed

--- multildap/client.py
+++ multildap/client.py
@@ -50,7 +50,7 @@ class LdapClient(object):
     def ensure_connection(self):
         search_kwargs = copy.deepcopy(self.conf['search'])
         search_kwargs['size_limit'] = 1
-        search_kwargs['timeout'] = self.conf['server']['connect_timeout']
+        search_kwargs['connect_timeout'] = self.conf['server']['connect_timeout']
         if self.conn and not self.conn.closed:
             try:
                 self.conn.search(**search_kwargs)
